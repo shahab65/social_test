7712c3ff0c65a2341c64b246df5c3c02bd3d80a3
shahab <shahab.emami95@gmail.com>
2021-12-12 00:29:06 +0330

    removing extra components

--- src/pages/Test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Test = () => {
-  return <div>this is test</div>;
-};
-
-export default Test;
--- src/routes/RouteList.tsx
+++ src/routes/RouteList.tsx
@@ -1,25 +1,13 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 
 // components
-import Login from "../pages/Login";
+import UserAccount from "../pages/UserAccount";
 import NotFound from "../pages/NotFound";
-import AddInstagramPage from "pages/AddInstagramPage";
-import ConfirmInstagramPage from "pages/VerifyInstagramPage";
-import Dashboard from "pages/Dashboard";
-import SelectCategory from "pages/SelectCategory";
 
 const RouteList = () => {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/add-instagram-page" element={<AddInstagramPage />} />
-      <Route
-        path="/confirm-instagram-page"
-        element={<ConfirmInstagramPage />}
-      />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/select-category" element={<SelectCategory />} />
+      <Route path="/" element={<UserAccount />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
