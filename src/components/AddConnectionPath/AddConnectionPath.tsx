a8198e80a5b5c60e36e84bac59cb1df3050413be
shahab <shahab.emami95@gmail.com>
2021-12-13 22:55:02 +0330

    using collapse instead of accordion

--- src/components/AddConnectionPath/AddConnectionPath.tsx
+++ src/components/AddConnectionPath/AddConnectionPath.tsx
@@ -1,30 +1,31 @@
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
 import AddConnectionPathForm from "./AddConnectionPathForm";
+import Collapse from "@mui/material/Collapse";
+import useToggle from "hooks/useToggle";
+
 import { Plus } from "components/icons";
 type Props = {};
 
 const AddConnectionPath = (props: Props) => {
   const {} = props;
+  const [isFormOpen, setIsFormOpen] = useToggle();
+  const onToggleForm = () => {
+    setIsFormOpen();
+  };
   return (
-    <Accordion>
-      <AccordionSummary>
-        <Typography sx={{}}>
-          <Plus />
-          افزودن مسیر ارتباطی
-        </Typography>
-      </AccordionSummary>
-      <AccordionDetails>
-        <Box>
-          <Box sx={{ p: 2, background: "red" }}>
-            <AddConnectionPathForm />
-          </Box>
+    <Box>
+      <Typography sx={{}} onClick={onToggleForm}>
+        <Plus />
+        افزودن مسیر ارتباطی
+      </Typography>
+
+      <Collapse in={isFormOpen}>
+        <Box sx={{ p: 2, background: "red" }}>
+          <AddConnectionPathForm />
         </Box>
-      </AccordionDetails>
-    </Accordion>
+      </Collapse>
+    </Box>
   );
 };
 export default AddConnectionPath;
