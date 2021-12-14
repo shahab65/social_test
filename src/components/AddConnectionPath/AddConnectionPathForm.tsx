1f74ea654176cf0c692b48ea59490163884764e0
shahab <shahab.emami95@gmail.com>
2021-12-14 18:08:21 +0330

    rewring with use formik hook

--- src/components/AddConnectionPath/AddConnectionPathForm.tsx
+++ src/components/AddConnectionPath/AddConnectionPathForm.tsx
@@ -8,6 +8,7 @@ import { Formik } from "formik";
 import linkOptions from "constants/linkOptions";
 import Axios from "api/Axios";
 import { useMutation } from "react-query";
+import { useFormik } from "formik";
 
 type Props = {
   onToggleForm: () => void;
@@ -16,6 +17,27 @@ type Props = {
 
 const AddConnectionPathForm = (props: Props) => {
   const { onToggleForm, refetchSocialLinks } = props;
+  const formik = useFormik({
+    initialValues: { type: "", social_link: "", social_id: "" },
+    onSubmit: (values) => {
+      mutation.mutate(values);
+    },
+  });
+  const {
+    values,
+    errors,
+    touched,
+    handleChange,
+    handleBlur,
+    handleSubmit,
+    isSubmitting,
+    resetForm,
+    /* and other goodies */
+  } = formik;
+  const onClose = () => {
+    resetForm();
+    onToggleForm();
+  };
   const mutation = useMutation(
     (sc: { social_link: string; social_id: string }) => {
       return Axios.post("/socials", sc);
@@ -23,100 +45,73 @@ const AddConnectionPathForm = (props: Props) => {
     {
       onSuccess: () => {
         refetchSocialLinks();
+        onClose();
       },
     }
   );
   return (
     <Box sx={{ p: 2, border: "1px solid black" }}>
       <Typography sx={{ fontSize: 14 }}>افزودن مسیر ارتباطی</Typography>
-      <Formik
-        initialValues={{ type: "", social_link: "", social_id: "" }}
-        validate={(values) => {
-          const errors = {};
-          return errors;
-        }}
-        onSubmit={(values) => {
-          mutation.mutate(values);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          handleReset,
-          /* and other goodies */
-        }) => {
-          const onCancelClick = () => {
-            handleReset();
-            onToggleForm();
-          };
-          return (
-            <form onSubmit={handleSubmit}>
-              <Stack
-                direction="row"
-                justifyContent="space-evenly"
-                alignItems="center"
-                spacing={1}
-                mb={2}
-              >
-                <TextField
-                  label="نوع"
-                  variant="outlined"
-                  select
-                  fullWidth
-                  value={values.type}
-                  name={"type"}
-                  onChange={handleChange}
-                >
-                  {linkOptions.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
 
-                <TextField
-                  label="لینک"
-                  variant="outlined"
-                  fullWidth
-                  name={"social_link"}
-                  value={values.social_link}
-                  onChange={handleChange}
-                />
+      <form onSubmit={handleSubmit}>
+        <Stack
+          direction="row"
+          justifyContent="space-evenly"
+          alignItems="center"
+          spacing={1}
+          mb={2}
+        >
+          <TextField
+            label="نوع"
+            variant="outlined"
+            select
+            fullWidth
+            value={values.type}
+            name={"type"}
+            onChange={handleChange}
+          >
+            {linkOptions.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
 
-                <TextField
-                  label="آی دی (ID)"
-                  variant="outlined"
-                  fullWidth
-                  name={"social_id"}
-                  value={values.social_id}
-                  onChange={handleChange}
-                />
-              </Stack>
-              <Stack direction="row-reverse" spacing={1}>
-                <Button
-                  // onClick={onToggleForm}
-                  variant="contained"
-                  type={"submit"}
-                >
-                  ویرایش مسیر ارتباطی تویتر
-                </Button>
-                <Button
-                  // onClick={onToggleForm}
-                  variant="outlined"
-                  onClick={onCancelClick}
-                >
-                  انصراف
-                </Button>
-              </Stack>
-            </form>
-          );
-        }}
-      </Formik>
+          <TextField
+            label="لینک"
+            variant="outlined"
+            fullWidth
+            name={"social_link"}
+            value={values.social_link}
+            onChange={handleChange}
+          />
+
+          <TextField
+            label="آی دی (ID)"
+            variant="outlined"
+            fullWidth
+            name={"social_id"}
+            value={values.social_id}
+            onChange={handleChange}
+          />
+        </Stack>
+        <Stack direction="row-reverse" spacing={1}>
+          <Button
+            // onClick={onToggleForm}
+            variant="contained"
+            type={"submit"}
+          >
+            ویرایش مسیر ارتباطی تویتر
+          </Button>
+          <Button
+            // onClick={onToggleForm}
+            variant="outlined"
+            onClick={onClose}
+          >
+            انصراف
+          </Button>
+        </Stack>
+      </form>
     </Box>
   );
 };
